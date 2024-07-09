07d91664ce49253578e4c55c62e4a6cfbaa91462
NogueiraNoob <cleiton.r.nogueira@gmail.com>
2024-07-09 12:58:14 -0300

    ajustes e testes portfolio

---
+++ DevCompleto/modulo04/tipos.go
@@ -0,0 +1,17 @@
+package main
+
+import(
+//		"fmt"
+)
+
+//func main()  {
+	// bool (trie/false)
+	//fmt.Printf("Type: %T - Value: %v\n", false, false)
+	// 
+	//fmt.Println("Type: %T - value %v\n", "steph", "steph")
+	//fmt.Println("Type: %T - value %v\n", 1, 1)
+	//fmt.Println("Type: %T - value %v\n", "1", "1")
+	//fmt.Println("Type: %T - value %v\n", "steph", "steph")
+	//fmt.Println("Type: %T - value %v\n", 1.123, 1.123)
+	
+//}
---
+++ DevCompleto/modulo04/varconst.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var nome string
+	var nome = "Bento"
+	fmt.Println(nome)
+}
